4a0deaf9ed99f7e27bb08bc56d828993245dfd5c
hanyucd <1807003141@qq.com>
2025-11-24 16:00:16 +0800

    feat: 完善图片选择组件

--- src/components/ImageUpload/ImageUpload.vue
+++ src/components/ImageUpload/ImageUpload.vue
@@ -3,9 +3,16 @@
   <view class="upload-module">
     <view v-for="(item, index) in imageList" :key="index" style="width: 230rpx;" class="image-item">
       <image :src="item" class="image" @click="previewImage(item)" />
-      
+      <!-- 移除 icon -->
       <view class="remove-icon-wrap" @click="removeImage(index)">
         <text class="remove-icon iconfont">&#xe620;</text>
+      </view>
+    </view>
+
+    <!-- 上传 upload -->
+    <view v-if="imageList.length < imgLimit" class="image-upload" @click="chooseImage">
+      <view class="upload-wrap">
+        <text class="icon-upload">+</text>
       </view>
     </view>
   </view>
@@ -16,19 +23,23 @@ const props = defineProps<{
   imageList: string[];
 }>();
 
+const imgLimit = ref(9);
+
+const emit = defineEmits<{
+  updateImage: [images: string[]];
+}>();
+
 /**
  * 选择图片
  */
-const chooseImage = () => {
-  uni.chooseImage({
-    // count: 9,
-    // sizeType: ['original', 'compressed'],
-    // sourceType: ['album', 'camera'],
-    // success: (res) => {
-    //   const tempFilePaths = res.tempFilePaths;
-    //   props.imageList.push(...tempFilePaths);
-    // },
-  });
+const chooseImage = async () => {
+  try {
+    const imgRes = await uni.chooseImage({ count: imgLimit.value - props.imageList.length });
+    const tempFilePaths = imgRes.tempFilePaths;
+    emit('updateImage', [...props.imageList, ...tempFilePaths]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -42,6 +53,9 @@ const previewImage = (item: string) => {
  * 移除图片
  */
 const removeImage = (index: number) => {
+  const newImgList = [...props.imageList];
+  newImgList.splice(index, 1);
+  emit('updateImage', newImgList);
 };
 </script>
 
